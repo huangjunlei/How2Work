0587957c60ebd3981752555fe7386c1cf09d9c92
huangjunlei <junlei-huang@outlook.com>
2019-02-20 14:34:35 +0800

    modify the type

--- DevOps/cmakedemo/demo3/src/Employee.cpp
+++ DevOps/cmakedemo/demo3/src/Employee.cpp
@@ -2,7 +2,7 @@
 #include "Employee.h"
 using namespace std;
 
-Employee::Employee(string name, int32_t id):name(name),id(id){}
+Employee::Employee(string name, int id):name(name),id(id){}
 
 void Employee::display(){
 	cout << "A employee with name " << this->name << " id " << this->id<< endl;
